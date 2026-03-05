94c5496705a645e005c7b98ba0fade63c9a935d3
fit-s-u-m <nehemiahmaths@gmail.com>
2026-03-05 11:56:12 +0300

    feat: added a way to install aur packages

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,9 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.packages" | grep -v '^$')
 sudo pacman -S --noconfirm --needed "${packages[@]}"
+
+
+# Install all aur packages
+mapfile -t aur_packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.aurPackages" | grep -v '^$')
+
+yay -S --noconfirm --needed "${aur_packages[@]}"
